52715f499f7fe31e36f46a127cdcb649a8bb0f0d
misuchiru <perryhaha@gmail.com>
2016-09-21 17:41:31 +0800

    完成 EX2 練習

--- ex2/books.rb
+++ ex2/books.rb
@@ -1,2 +1,19 @@
 require 'nokogiri'
 require 'open-uri'
+
+books = Nokogiri::HTML(open('http://www.books.com.tw/activity/gold66_day/?loc=activity_BK_001'))
+
+puts "### 搜尋..."
+
+=begin
+# 爬66折
+books.css('div h1 a').each do |a|
+	puts a.content
+end
+=end
+
+#爬本月新加入
+
+books.css('div.sec_1 a').each do |a|
+	puts a.text()
+end
